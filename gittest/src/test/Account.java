0066497bf1442d8beac38cbacff11073c7164690
jungsungwon <jsw302@naver.com>
2022-03-28 16:54:24 +0900

    withdraw 추가

--- gittest/src/test/Account.java
+++ gittest/src/test/Account.java
@@ -10,4 +10,11 @@ public class Account {
 
 	private String ano;
 	private int balance;
+	
+	public void deposit() {
+		System.out.println("deposit");
+	}
+	public void withdraw() {
+		System.out.println("withdraw");
+	}
 }
