c3dd1eeb5ab5d626435973f4a4173b156830563d
jungsungwon <jsw302@naver.com>
2022-04-07 18:17:04 +0900

    jsp study1

--- practice/src/practice/CreateDiamond.java
+++ practice/src/practice/CreateDiamond.java
@@ -3,13 +3,37 @@ package practice;
 public class CreateDiamond {
 	public static void main(String[] args) {
 		for(int i=0; i<7; i++) {
-			for(int j=0; j<7; j++ ) {
-				System.out.print(" ■ ");
-				if()
-				}
-			}
+				for(int j=7; j>0; j--) {
+					if(i>=j) {
+						System.out.print(" * ");
+					}   else  {
+						System.out.print("   ");
+					}
+				} for(int j=0; j<7; j++ ) {
+					if(i<j) {
+						System.out.print("   ");
+					}else {
+						System.out.print(" * ");
+					}
+				} 
 			System.out.println();
+
+				for(int k=0; k<7; k++) {
+					if(k<i) {
+						System.out.print("   ");
+					} else {
+						System.out.print(" 1 ");
+					}
+				}
+//				for(int l=7)
+				System.out.println();
+			}
+		System.out.println();
 		}
 
-	}
+	
 }
+/*
+ * for(int i = 0; i < x ; i++){}
+ * for(int i = x; i >=0; i--){}
+ */
---
+++ web1/src/main/webapp/footer.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <% int age2 = 30; %>
+<div class="footer">
+	<h3>회사정보</h3>
+</div>
+
+</body>
+</html>
